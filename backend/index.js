0e0f8c1bb6669eb43bdb848f493f8f805db47d34
Harshita <harshitamaheshwari762@gmail.com>
2026-02-03 18:56:16 +0530

    Added

--- backend/index.js
+++ backend/index.js
@@ -124,8 +124,7 @@ app.post("/api/transfer", async (req, res) => {
   }
 });
 
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;  
 app.listen(PORT, () => {
-  console.log("Backend running on port", PORT);
+  console.log(`Backend running on port ${PORT}`);
 });
